c3c8e80e5ff2e218af41e7812652f50de6d48217
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2025-04-15 14:35:54 -0300

    .

--- sprint2/cadastro-funcionario.php
+++ sprint2/cadastro-funcionario.php
@@ -6,41 +6,49 @@
     <title>Cadastro Funcionário</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<style>
+    .titulo{
+        margin: 0 90px 20px; 
+        font-weight: bold;
+    }
+</style>
+<body id="login">
     <div class="d-flex col-sm-12">
-        <div class="col-sm-5"></div>
-        <div class="container col-sm-7 mt-2 d-flex flex-column align-items-center">
-            <h1 class="align-self-start titulo fw-bold">Funcionário</h1>
-            <p class="align-self-start fw-normal">Seja bem vindo(a)! Realize seu cadastro!</p>
-            <form action="" class="col-sm-8">
+        <div class="col-sm-5" id="fundo"></div>
+        <div class="container col-sm-7 d-flex flex-column justify-content-center align-items-center">
+            <div class="align-self-start titulo">
+                <h1 class="mb-2">Funcionário</h1>
+                <p id="bem-vindo">Seja bem vindo(a)! Realize seu cadastro!</p>
+            </div>
+            <form action="" class="col-sm-9">
                 <div>
-                    <label for="nome" class="fw-normal mt-1">Nome completo:</label>
+                    <label for="nome" class="fw-normal">Nome completo:</label>
                     <input type="text" name="nome_funcionario" id="nome_funcionario" required class="form-control shadow-sm" placeholder="Digite seu nome">
                 </div>
                 <div>
-                    <label for="email" class="fw-normal mt-1">Email:</label>
+                    <label for="email" class="fw-normal">Email:</label>
                     <input type="email" name="email_funcionario" id="email_funcionario" required class="form-control shadow-sm" placeholder="Digite seu email">
                 </div>
                 <div>
-                    <label for="senha" class="fw-normal mt-1">Senha:</label>
+                    <label for="senha" class="fw-normal">Senha:</label>
                     <input type="password" min="8" name="senha_funcionario" id="senha_funcionario" required class="form-control shadow-sm" placeholder="Digite sua senha">
                 </div>
                 <div>
-                    <label for="cpf" class="fw-normal mt-1">CPF:</label>
+                    <label for="cpf" class="fw-normal">CPF:</label>
                     <input type="number" min="11" name="cpf" id="cpf" required class="form-control shadow-sm" placeholder="Digite seu CPF">
                 </div>
                 <div>
-                    <label for="atuacao" class="fw-normal mt-1">Área de atuação</label>
-                    <input type="text" name="atuacao" id="atuacao" required class="form-control shadow-sm" placeholder="Digite sua area de atuação(Ex. Engenheiro, Medico e etc">
+                    <label for="atuacao" class="fw-normal">Área de atuação</label>
+                    <input type="text" name="atuacao" id="atuacao" required class="form-control shadow-sm" placeholder="Digite sua area de atuação(Ex. Engenheiro, Medico e etc)">
                 </div>
                 <div>
-                    <label for="servico" class="fw-normal mt-1">Valor de seu serviço</label>
+                    <label for="servico" class="fw-normal">Valor de seu serviço</label>
                     <input type="text" name="servico" id="servico" required class="form-control shadow-sm" placeholder="Digite o valor do seu seviço">
                 </div>
                 <div>
                     <p class="fw-normal">Anexe seu currículo <button class="btn btn-submit btn-sm">Adicionar Arquivo</button></p>
                 </div>
-                <button class="btn btn-submit btn-primary w-100 mt-1">cadastrar-se</button>
+                <button class="btn btn-submit w-100" id="btn-custom">cadastrar-se</button>
             </form>
         </div>
     </div>
