6bffa88c4b37d86c64bd4132ef82eccc7149343b
Gustavo <gustavorsenai@gmail.com>
2025-04-15 14:22:37 -0300

    login

--- sprint2/Login.php
+++ sprint2/Login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -8,25 +7,29 @@
     <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
-<body class="d-flex">
-    <div class="container" id="fundo">
+<body>
+    <div class="d-flex col-sm-12 " >
+        <div class="container col-sm-5" id="fundo">
+        </div>
+        <main class="container col-sm-7 mt-8 d-flex align-items-center justify-content-center flex-column" id="login">
+            <div class="align-self-start titulo">
+                <h1 class="mb-2">Login</h1>
+                <p id="bem-vindo">Seja bem vindo(a)! Faça seu login abaixo.</p>
+            </div>
+            <form action="#" method="POST" class="form col-sm-9">
+                <div>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" class="form-control input-custom shadow-sm" required />
+                </div>
+                <div>
+                    <label for="senha">Senha:</label>
+                    <input type="password" id="senha" name="senha" class="form-control input-custom shadow-sm"  required />
+                </div>
+                <button type="submit" class="btn btn-submit mt-3 mb-3 w-100 " id="btn-custom">Entrar</button>
+            </form>
+            <p id="senha-esqueceu">Não tem uma conta? <a href="cadastro.html">Cadastre-se</a></p>
+        </main>
     </div>
-    <main class="container">
-    <main class="container" id="login">
-        <h1>Login</h1>
-        <form action="#" method="POST" class="form col-sm-6">
-        <form action="php-t.php" method="POST" class="form col-sm-6">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" class="form-control" required />
-
-            <label for="senha">Senha:</label>
-            <input type="password" id="senha" name="senha" class="form-control"  required />
-
-            <button type="submit" class="btn btn-sm btn-success mt-3">Entrar</button>
-        </form>
-        <p>Não tem uma conta? <a href="cadastro.html">Cadastre-se</a></p>
-    </main>
-
     <script src="script.js"></script>
 </body>
 </html>
