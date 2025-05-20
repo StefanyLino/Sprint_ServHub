1d594cc663a2eed1b105539c748bbd4f4b36f995
Gustavo <gustavorsenai@gmail.com>
2025-05-20 09:20:26 -0300

    comitado

--- sprint3/public/cadastro-funcionario.php
+++ sprint3/public/cadastro-funcionario.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <title>Cadastro Funcionário</title>
-    <link rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.12.1/font/bootstrap-icons.min.css">
-</head>
+<?php
+    include 'htmls/head.html';
+?>
 <body id="login">
     <div class="d-flex col-sm-12">
         <div class="col-sm-6" id="fundo"></div>
